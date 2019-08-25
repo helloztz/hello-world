8f6d08f241f7a2c89e31c7dfb1df2bdbf3971471
Chenruixin <2416020591@qq.com>
2019-08-25 10:03:56 +0800

    重新设置appId

--- project/src/utils/setAppId.js
+++ project/src/utils/setAppId.js
@@ -1,8 +1,8 @@
 import settings from '@/settings.js'
 
 let setAppId = () => {
-    // settings.global.appId=window.location.href.split('//')[1].split('.')[0];
-    settings.global.appId = 'wx129eaf6876332fba';
+    settings.global.appId=window.location.href.split('//')[1].split('.')[0];
+    // settings.global.appId = 'wx129eaf6876332fba';
 }
 
 export default setAppId;
