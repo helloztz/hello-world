b9c8e85577623b47a57b9cf1732d0fa0ff9c7e5f
王利民 <17629094718@163.com>
2019-09-21 16:38:49 +0800

    * 放开路由拦截注释

--- project/src/router/permissions.js
+++ project/src/router/permissions.js
@@ -1,20 +1,20 @@
 import router from './index.js'
 import {getUserInfor} from '@/utils/index.js'
 import settings from '@/settings.js'
-// router.beforeEach((to, from ,next) => {
-//   let isLogin = getUserInfor()
-//   let whiteList = settings.router.whiteList
-//   if (!isLogin) { // 未登录
-//     if (whiteList.includes(to.path)) {
-//       next()
-//     } else {
-//       next('Login')
-//     }
-//   } else {
-//     if (to.path === '/Login') {
-//       next('/')
-//     } else {
-//       next()
-//     }
-//   }
-// })
+router.beforeEach((to, from ,next) => {
+  let isLogin = getUserInfor()
+  let whiteList = settings.router.whiteList
+  if (!isLogin) { // 未登录
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      next('Login')
+    }
+  } else {
+    if (to.path === '/Login') {
+      next('/')
+    } else {
+      next()
+    }
+  }
+})
